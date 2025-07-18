d86c1c1cf2abb99ec7d39fafcf209ddd140e7cba
George <49768197+thecapibara@users.noreply.github.com>
2025-07-18 19:50:41 +0300

    REVERT VINTF #1-2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,9 +46,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := \
-    hardware/qcom-caf/common/compatibility_matrix.xml \
-    hardware/motorola/configs/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Kernel
