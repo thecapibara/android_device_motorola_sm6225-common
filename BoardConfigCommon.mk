fedcecba8f391871c30b06301fff95e4a908ac1e
George <49768197+thecapibara@users.noreply.github.com>
2025-07-18 19:02:54 +0300

    RIL FIX #1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,7 +46,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := \
+    hardware/qcom-caf/common/compatibility_matrix.xml \
+    $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Kernel
