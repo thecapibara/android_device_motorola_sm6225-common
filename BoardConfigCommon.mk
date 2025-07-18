6aac9c8bc4754f950ecbafa493a14d6da43973c1
George <49768197+thecapibara@users.noreply.github.com>
2025-07-18 19:41:41 +0300

    VINTF FIX #2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := \
     hardware/qcom-caf/common/compatibility_matrix.xml \
-    $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+    hardware/motorola/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Kernel
